4fd131527f1c38e800a3b68396e258cf32672d10
shruti0190 <91966500+shruti0190@users.noreply.github.com>
2021-10-30 15:05:31 +0530

    assignment2-Q2

---
+++ idCardGeneration.py
@@ -0,0 +1,9 @@
+def my_function(name, phone, adress):
+    print("---Employee ID Card---")
+    print("The name is " + name)
+    print("The phone is " + phone)
+    print("The adress is " + adress)
+
+
+my_function(name = "shruti", phone = "789456123", adress = "home")
+
